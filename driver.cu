36bfce8b29cb2f918e52b623223219ff9a1236ee
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2021-01-06 17:59:14 +0100

    Test the MiraCle library

--- driver.cu
+++ driver.cu
@@ -5,7 +5,31 @@
  *
  */
 
-int main(int argc, char *argv[])
-{
+
+#include <stdio.h>
+
+
+#include "miracle.h"
+
+
+// number of program parameters
+#define NUM_PARAMS 2
+
+
+int main(int argc, char *argv[]) {
+    if(argc != NUM_PARAMS) {
+        fprintf(stderr, "usage: %s filename\n", argv[0]);
+        return 1;
+    }
+
+    char *filename = argv[1];
+    int err = new_miracle(filename);
     
+    if(err) {
+        return err;
+    }
+
+    delete_miracle();
+
+    return 0;
 }

4efc5d4103a8afdd287ea62816ea5b7926dba865
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2021-01-06 18:23:48 +0100

    Add miracle.h

---
+++ miracle.h
@@ -0,0 +1,17 @@
+/*
+ * miracle.h
+ *
+ * file containing the MiraCle library API
+ *
+ */
+
+
+#ifndef MIRACLE
+#define MIRACLE
+
+
+int new_miracle(char *filename);
+void delete_miracle();
+
+
+#endif

00284ab55bac0dbd182443d79dc6a0e917a9a9b2
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2021-01-13 14:46:57 +0100

    Add new_lit_assignments and backjumping functions

--- formula.h
+++ formula.h
@@ -13,6 +13,8 @@
 int new_formula(char *filename);
 void delete_formula();
 void print_formula();
+void new_lit_assignments(int *new_lit_ass, unsigned int new_lit_ass_len);
+void backjumping(unsigned int bj_dec_lvl);
 
 
 #endif

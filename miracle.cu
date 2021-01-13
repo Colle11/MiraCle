6256ca65146aa66ea8eb07a120f96c3e9a9c1b91
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2021-01-13 16:05:36 +0100

    Test the formula library

--- miracle.cu
+++ miracle.cu
@@ -42,6 +42,19 @@ int new_miracle(char *filename) {
     if(err) {
         return err;
     }
+    
+    // DEBUG
+    int new_lit_ass_fst[] = {1};
+    new_lit_assignments(new_lit_ass_fst, 1);
+    int new_lit_ass_snd[] = {-3};
+    new_lit_assignments(new_lit_ass_snd, 1);
+    int new_lit_ass_trd[] = {33, -18, -45, 94, 58, 56, -65, 30, -48, -77};
+    new_lit_assignments(new_lit_ass_trd, 10);
+    backjumping(1);
+    int new_lit_ass_frt[] = {-72, 20, 68, -54, 59, -57, 89, 98, -96, 83};
+    new_lit_assignments(new_lit_ass_frt, 10);
+    backjumping(0);
+    // END DEBUG
 
     print_formula();
 

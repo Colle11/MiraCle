214ebf756cfdd1faa1f250b20a4e571c569b38a8
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2021-02-01 12:37:19 +0100

    Add heuristics to the MiraCle library API

--- miracle.h
+++ miracle.h
@@ -10,8 +10,24 @@
 #define MIRACLE
 
 
+/*
+ * API function prototypes
+ */
+
 int new_miracle(char *filename);
 void delete_miracle();
+int DLIS_heuristic();
+int DLCS_heuristic();
+int RDLIS_heuristic();
+int RDLCS_heuristic();
+int JW_OS_heuristic();
+int JW_TS_heuristic();
+int MOM_heuristic(unsigned int k);
+int BOHM_heuristic(unsigned int alpha, unsigned int beta);
+int RAND_heuristic();
+
+
+/*****************************************************************************/
 
 
 #endif

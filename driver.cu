65c6eef270e2c681c71b2c0e9d3fd5ab1b6bd39c
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2021-02-01 12:29:59 +0100

    Debug heuristics

--- driver.cu
+++ driver.cu
@@ -29,6 +29,18 @@ int main(int argc, char *argv[]) {
         return err;
     }
 
+    // DEBUG
+    printf("DLIS = %i\n", DLIS_heuristic());
+    printf("RDLIS = %i\n", RDLIS_heuristic());
+    printf("DLCS = %i\n", DLCS_heuristic());
+    printf("RDLCS = %i\n", RDLCS_heuristic());
+    printf("JW_OS = %i\n", JW_OS_heuristic());
+    printf("JW_TS = %i\n", JW_TS_heuristic());
+    printf("MOM = %i\n", MOM_heuristic(3));
+    printf("BOHM = %i\n", BOHM_heuristic(1, 2));
+    printf("RAND = %i\n", RAND_heuristic());
+    // END DEBUG
+
     delete_miracle();
 
     return 0;

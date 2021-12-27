839461cf742577769a1966d36c42decd0712a1b4
Michele Collevati <11272443+Colle11@users.noreply.github.com>
2021-12-27 16:41:40 +0100

    Disables GPU error checking.

--- src/utils.cuh
+++ src/utils.cuh
@@ -47,7 +47,8 @@ static bool init_PRNG_seed = false;     /**
  * @param [in]ans A CUDA API call.
  * @retval None.
  */
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+// #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+#define gpuErrchk(ans) { (ans); }
 
 
 /**
